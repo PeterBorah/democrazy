2a94e58be70d82f5be993abc79a50170a537decf
Peter Borah <peterborah@gmail.com>
2015-08-22 15:09:16 -0400

    Run rounds and hand out winnings.

--- contracts/Democrazy.sol
+++ contracts/Democrazy.sol
@@ -2,9 +2,13 @@ contract Democrazy {
   address[] public players;
   mapping(address => bytes32) public commitments;
   mapping(address => uint8) public votes;
+  mapping(address => uint) public balances;
+  uint[3] public total_votes;
+  uint public round;
 
   function add_player(address _player) {
     players[players.length++] = _player;
+    balances[_player] = 10000;
   }
 
   function num_players() returns(uint) {
@@ -26,6 +30,45 @@ contract Democrazy {
 
     if (sha256(vote_string, seed) == commitments[msg.sender]) {
       votes[msg.sender] = vote;
+      total_votes[vote] += 1;
     }
   }
+
+  function end_round() {
+    uint8 majority;
+    uint8 minority;
+    uint8 winning_team;
+
+    if (total_votes[1] > total_votes[2]) {
+      majority = 1;
+      minority = 2;
+    } else {
+      majority = 2;
+      minority = 1;
+    }
+    
+    if (round % 2 == 1) {
+      winning_team = majority;
+    } else {
+      winning_team = minority;
+    }
+
+    uint prize_value = (players.length * 100) / total_votes[winning_team];
+      
+    for (uint i; i < players.length; i++) {
+      address player = players[i];
+      if (votes[player] == winning_team) {
+        balances[player] += prize_value - 100;
+      } else {
+        balances[player] -= 100;
+      }
+
+      votes[player] = 0;
+      commitments[player] = "";
+    }
+    
+    delete total_votes;
+    round++;
+  }
+
 }
--- test/democrazy.js
+++ test/democrazy.js
@@ -40,4 +40,32 @@ contract('Democrazy', function(accounts) {
         done();
     }).catch(done);
   });
+
+  it("should reward according to the round", function(done) {
+    var commitment_1 = "0xf25fe1d943db0d3853b6931c487128a31c8adb972e15da792f090739ba3f1e06"; //sha256 of "1foo"
+    var commitment_2 = "0x377195904c99497c2cdb7aaecaf541ca717f34e5357dace55ebb1711d54190c2"; //sha256 of "2foo"
+
+    Democrazy.new().
+    then(function(democrazy) {
+      democrazy.add_player(accounts[0]).
+        then(function() { return democrazy.commit(commitment_1) }).
+        then(function() { return democrazy.add_player(accounts[1]) }).
+        then(function() { return democrazy.commit(commitment_1, {from: accounts[1]}) }).
+        then(function() { return democrazy.add_player(accounts[2]) }).
+        then(function() { return democrazy.commit(commitment_2, {from: accounts[2]}) }).
+        then(function() { return democrazy.reveal(1, "foo") }).
+        then(function() { return democrazy.reveal(1, "foo", {from: accounts[1]}) }).
+        then(function() { return democrazy.reveal(2, "foo", {from: accounts[2]}) }).
+        then(function() { return democrazy.end_round() }).
+        then(function() { return democrazy.balances.call(accounts[0]) }).
+        then(function(result) { assert.equal(result, 9900) }).
+        then(function() { return democrazy.balances.call(accounts[1]) }).
+        then(function(result) { assert.equal(result, 9900) }).
+        then(function() { return democrazy.balances.call(accounts[2]) }).
+        then(function(result) {
+          assert.equal(result, 10200)
+          done();
+      }).catch(done)
+    }).catch(done)
+  });
 });
