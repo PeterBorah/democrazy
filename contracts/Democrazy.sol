35edf7ee27a1aa4172b198816c325f84a1af490d
Peter Borah <peterborah@gmail.com>
2015-08-22 11:51:07 -0400

    Let players add commitments.

--- contracts/Democrazy.sol
+++ contracts/Democrazy.sol
@@ -1,5 +1,6 @@
 contract Democrazy {
   address[] public players;
+  mapping(address => bytes32) public commitments;
 
   function add_player(address _player) {
     players[players.length++] = _player;
@@ -7,5 +8,9 @@ contract Democrazy {
 
   function num_players() returns(uint) {
     return players.length;
+  }
+
+  function commit(bytes32 commitment) {
+    commitments[msg.sender] = commitment;
   }
 }
--- test/democrazy.js
+++ test/democrazy.js
@@ -11,4 +11,17 @@ contract('Democrazy', function(accounts) {
         done();
     }).catch(done);
   });
+
+  it("should let players add their commitments", function(done) {
+    var democrazy = Democrazy.at(Democrazy.deployed_address);
+    var commitment = 0xf25fe1d943db0d3853b6931c487128a31c8adb972e15da792f090739ba3f1e06 //sha256 of "1foo"
+
+    democrazy.add_player(accounts[0]).
+      then(function() { return democrazy.commit(commitment) }).
+      then(function() { return democrazy.commitments.call(accounts[0]) }).
+      then(function(result) {
+        assert.equal(result, commitment);
+        done();
+    }).catch(done);
+  });
 });
